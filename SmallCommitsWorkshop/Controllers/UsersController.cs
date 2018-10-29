0f9f6cd2967fae4a8853ca1369a5247d5559d51c
Mark Tse <mark.tse@d2l.com>
2018-10-29 15:33:42 -0400

    users - adding big PR.

--- SmallCommitsWorkshop/Controllers/UsersController.cs
+++ SmallCommitsWorkshop/Controllers/UsersController.cs
@@ -14,8 +14,8 @@ namespace SmallCommitsWorkshop.Controllers {
 			m_usersContext = usersContext;
 
 			if( !m_usersContext.Users.Any() ) {
-				m_usersContext.Users.Add( new User() { Id = 169, UserName = "D2LSupport" } );
-				m_usersContext.Users.Add( new User() { Id = 175, UserName = "user1" } );
+				m_usersContext.Users.Add( new User() { Id = 169, UserName = "D2LSupport", IsActive = true } );
+				m_usersContext.Users.Add( new User() { Id = 175, UserName = "user1", IsActive = false } );
 				m_usersContext.SaveChanges();
 			}
 		}
@@ -24,6 +24,32 @@ namespace SmallCommitsWorkshop.Controllers {
 		public async Task<ActionResult<IDictionary<long, User>>> GetAll() {
 			IEnumerable<User> users = await m_usersContext.Users.ToListAsync();
 			return users.ToDictionary( user => user.Id, user => user );
+		}
+
+		[HttpGet( "{userId}" )]
+		public async Task<ActionResult<IDictionary<long, User>>> Get( long userId ) {
+			User user = await m_usersContext.Users.FindAsync( userId );
+			if( user == null ) {
+				return NotFound();
+			}
+			return new Dictionary<long, User>( 1 ) { { user.Id, user } };
+		}
+
+		[HttpPost]
+		public async Task<ActionResult> CreateOrUpdate( [FromBody] User newUser ) {
+			User user = m_usersContext.Users.Find( newUser.Id );
+			ActionResult response;
+			if( user == null ) {
+				m_usersContext.Add( newUser );
+				response = Created( newUser.Id.ToString(), newUser );
+			} else {
+				user.UserName = newUser.UserName;
+				m_usersContext.Update( user );
+				response = NoContent();
+			}
+			
+			await m_usersContext.SaveChangesAsync();
+			return response;
 		}
 	}
 }
--- SmallCommitsWorkshopTests/Controllers/UsersControllerTests.cs
+++ SmallCommitsWorkshopTests/Controllers/UsersControllerTests.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.Testing;
@@ -16,13 +17,18 @@ namespace SmallCommitsWorkshopTests.Controllers {
 		private HttpClient m_client;
 		private IServiceScope m_scope;
 		private UsersContext m_usersContext;
+		private User[] m_defaultUsers = new User[] {
+			new User() { Id = 42L, UserName = "JaneSmith", IsActive = true },
+			new User() { Id = 156L, UserName = "JoeSmith", IsActive = false },
+		};
 
 		[SetUp]
-		public void SetUp() {
+		public async Task SetUp() {
 			m_factory = new WebApplicationFactory<Startup>();
 			m_client = m_factory.CreateClient();
 			m_scope = m_factory.Server?.Host.Services.CreateScope();
 			m_usersContext = m_scope.ServiceProvider.GetService<UsersContext>();
+			await AddUsers( m_defaultUsers );
 		}
 
 		[TearDown]
@@ -34,28 +40,83 @@ namespace SmallCommitsWorkshopTests.Controllers {
 
 		[Test]
 		public async Task GetAll_ReturnsUsers() {
-			User[] users = new User[] {
-				new User() { Id = 42, UserName = "JaneSmith" },
-				new User() { Id = 156, UserName = "JoeSmith" },
-			};
-
-			foreach( User user in users ) {
-				m_usersContext.Users.Add( user );
-			}
-			await m_usersContext.SaveChangesAsync();
-
 			using( HttpResponseMessage response = await m_client.GetAsync( "/api/users" ) ) {
 				CollectionAssert.AreEquivalent(
-					users.ToDictionary(
+					m_defaultUsers.ToDictionary(
 						user => user.Id,
 						user => new Dictionary<string, object>( 2 ) {
 							{ "id", user.Id },
 							{ "userName", user.UserName },
+							{ "isActive", user.IsActive },
 						}
 					),
 					await response.Content.ReadAsJsonAsync<IDictionary<long, IDictionary<string, object>>>()
 				);
 			}
+		}
+
+		[Test]
+		public async Task Get_ReturnsUser() {
+			User user = m_defaultUsers.First();
+			using( HttpResponseMessage response = await m_client.GetAsync( $"/api/users/{user.Id}" ) ) {
+				Assert.AreEqual( response.StatusCode, HttpStatusCode.OK );
+				CollectionAssert.AreEquivalent(
+					new Dictionary<long, IDictionary<string, object>>( 2 ) {
+						{
+							user.Id,
+							new Dictionary<string, object>( 2 ) {
+								{ "id", user.Id },
+								{ "userName", user.UserName },
+								{ "isActive", user.IsActive },
+							}
+						}
+					},
+					await response.Content.ReadAsJsonAsync<IDictionary<long, IDictionary<string, object>>>()
+				);
+			}
+		}
+
+		[Test]
+		public async Task CreateOrUpdate_UserDoesNotExist_CreatesUser() {
+			User user = new User { Id = 12456L, UserName = "AnotherUser", IsActive = true };
+
+			CollectionAssert.DoesNotContain(
+				m_defaultUsers.Select( u => u.Id ),
+				user.Id
+			);
+
+			using( HttpResponseMessage response = await m_client.PostAsJsonAsync( "/api/users/", user ) ) {
+				Assert.AreEqual( HttpStatusCode.Created, response.StatusCode );
+			}
+
+			User actualUser = await m_usersContext.Users.FindAsync( user.Id );
+			Assert.NotNull( actualUser, "Could not find created user!" );
+			Assert.AreEqual( user.UserName, actualUser.UserName );
+			Assert.AreEqual( user.IsActive, actualUser.IsActive );
+		}
+
+		[Test]
+		public async Task CreateOrUpdate_UserExists_UpdatesUser() {
+			const string newUserName = "NewUserName";
+			User user = m_defaultUsers.First();
+			Assert.AreNotEqual( newUserName, user.UserName );
+
+			User newUserDetails = new User() { Id = user.Id, UserName = newUserName, IsActive = !user.IsActive };
+			using( HttpResponseMessage response = await m_client.PostAsJsonAsync( "/api/users/", newUserDetails ) ) {
+				Assert.AreEqual( HttpStatusCode.NoContent, response.StatusCode );
+			}
+
+			await m_usersContext.Entry( user ).ReloadAsync();
+			User actualUser = await m_usersContext.Users.FindAsync( user.Id );
+
+			Assert.NotNull( actualUser, "Could not find created user!" );
+			Assert.AreEqual( newUserDetails.UserName, actualUser.UserName );
+			Assert.AreEqual( user.IsActive, actualUser.IsActive );
+		}
+
+		private Task AddUsers( params User[] users ) {
+			m_usersContext.Users.AddRange( users );
+			return m_usersContext.SaveChangesAsync();
 		}
 	}
 }
