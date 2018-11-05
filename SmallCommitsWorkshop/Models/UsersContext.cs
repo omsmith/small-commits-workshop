b0029c5691918daeec8d3ecba4ecdfec4e8a13db
Mark Tse <neverendingqs@users.noreply.github.com>
2018-11-05 16:03:11 -0500

    users - adding index on User.Id. (#1)

--- SmallCommitsWorkshop/Models/UsersContext.cs
+++ SmallCommitsWorkshop/Models/UsersContext.cs
@@ -6,5 +6,11 @@ namespace SmallCommitsWorkshop.Models {
 			: base( options ) { }
 
 		public DbSet<User> Users { get; set; }
+
+		protected override void OnModelCreating( ModelBuilder modelBuilder ) {
+			modelBuilder.Entity<User>()
+				.HasIndex( u => u.Id )
+				.IsUnique();
+		}
 	}
 }
